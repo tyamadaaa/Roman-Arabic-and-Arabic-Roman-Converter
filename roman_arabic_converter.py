eb7fc4fd31409de65347dd5533673022ba578d4b
Tomoka Yamada <37702849+tyamadaaa@users.noreply.github.com>
2018-04-25 21:08:50 +0800

    Update roman_arabic_converter.py

--- roman_arabic_converter.py
+++ roman_arabic_converter.py
@@ -5,13 +5,16 @@ answer = input("""Convert:
                   Please enter 1 or 2 : """)
 
 
-if answer == "1": #A user will need to write down an arabic number into the IDLE Shell
+if answer == "1":    
+    #A user will need to write down an arabic number into the IDLE Shell
     convert = input("Please enter an arabic number between 0 and 3999: ")
 
-elif answer == "2": #A user will need to write down roman numerals into the IDLE Shell
+elif answer == "2":  
+    #A user will need to write down roman numerals into the IDLE Shell
     convert = input("Please enter roman numerals between 0 and 3999: ")
 
-else: #A user need to type either 1 or 2 
+else:     
+    #A user need to type either 1 or 2 
     print("Please type 1 or 2 only")
 
 
@@ -27,17 +30,17 @@ def arabictoroman(convert):
     #The program runs when the arabic number is between 0 and 4000
     if int(convert) < 4000 and int(convert) > 0:
 
-        thousands = int(int(convert)/1000) #Finds the digit of thousand's place
-        output.append(roman_thousands[(thousands)]) #Adds the corresponding roman numerals into 'output'
+        thousands = int(int(convert)/1000)            #Finds the digit of thousand's place
+        output.append(roman_thousands[(thousands)])   #Adds the corresponding roman numerals into 'output'
 
-        hundreds = int(int(convert)/100 % 10) #Finds the digit of hundred's place
-        output.append(roman_hundreds[(hundreds)]) #Adds the corresponding roman numerals into 'output'
+        hundreds = int(int(convert)/100 % 10)         #Finds the digit of hundred's place
+        output.append(roman_hundreds[(hundreds)])     #Adds the corresponding roman numerals into 'output'
 
-        tens = int(int(convert)/10 % 10) #Finds the digit of ten's place
-        output.append(roman_tens[(tens)]) #Adds the corresponding roman numerals into 'output'
+        tens = int(int(convert)/10 % 10)              #Finds the digit of ten's place
+        output.append(roman_tens[(tens)])             #Adds the corresponding roman numerals into 'output'
 
-        ones = int(int(convert) % 10) #Finds the digit of one's place
-        output.append(roman_ones[(ones)]) #Adds the corresponding roman numerals into 'output'
+        ones = int(int(convert) % 10)                 #Finds the digit of one's place
+        output.append(roman_ones[(ones)])             #Adds the corresponding roman numerals into 'output'
 
     #Joining all roman numerals in the 'output' list and store it in 'whole'
     output = [str(i) for i in output]
@@ -48,20 +51,20 @@ def arabictoroman(convert):
 #Creating a function that converts roman numerals to arabic number
 def romantoarabic(convert):
 
-    subtract = [] #The numbers to be subtracted are stored here
-    new_output = [] #Finalized list of numbers
+    subtract = []     #The numbers to be subtracted are stored here
+    new_output = []   #Finalized list of numbers
 
     #A dictionary with roman numerals corresponding to arabic numbers
     arabic = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
 
-    output = list(convert) #Converts the roman numerals entered into a list
-    n = len(str(convert)) #Calculates the number of letters entered
+    output = list(convert)  #Converts the roman numerals entered into a list
+    n = len(str(convert))   #Calculates the number of letters entered
 
     #Taking two letters each time to compare the values
     for i in range(n-1):
-        first = str(convert[i]) #First letter taken is converted into a corresponding number
+        first = str(convert[i])          #First letter taken is converted into a corresponding number
         first_key = int(arabic[first])
-        second = str(convert[i+1]) #Second letter taken is converted into a corresponding number
+        second = str(convert[i+1])       #Second letter taken is converted into a corresponding number
         second_key = int(arabic[second])
         
         #If the first number is smaller than the second number
@@ -77,11 +80,11 @@ def romantoarabic(convert):
         for roman, numbers in arabic.items():
             if str(k) == roman:
                 new_k = int(numbers)
-        new_output.append(new_k) #The arabic number is added to the 'new_output' list 
+        new_output.append(new_k)    #The arabic number is added to the 'new_output' list 
 
-    sum_of_output = sum(new_output) #Takes the sum of the 'new_output' list
-    sum_of_subtract = sum(subtract) #Takes the sum of the 'subtract' list
-    arabics = sum_of_output + sum_of_subtract #Takes the sum of the above 2 lists 
+    sum_of_output = sum(new_output)            #Takes the sum of the 'new_output' list
+    sum_of_subtract = sum(subtract)            #Takes the sum of the 'subtract' list
+    arabics = sum_of_output + sum_of_subtract  #Takes the sum of the above 2 lists 
 
     return arabics #Returns the final answer 
 
